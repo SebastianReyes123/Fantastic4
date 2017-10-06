c209079216ec44b5bcf6b7378c25fe9194dce5b8
sebastian.reyes.c <sebastian.reyes.c@outlook.com>
2017-10-06 14:50:11 +0200

    Random ship klar

--- Fantastic4/JavaScript.js
+++ Fantastic4/JavaScript.js
@@ -1,81 +1,70 @@
-﻿//var ship1;
-//var ship2;
-//var ship3;
-//var ship4;
-//var ship5;
+﻿$(document).ready(function () {
 
-$(document).ready(function () {
-
-    $("#ButtonCreateBoardSmall").click(function () {
+	$("#ButtonCreateBoardSmall").click(function () {
 		var name = $("#Name").val();
-		
-		$("div").remove(".start");
-        CreateBoard(5);
-        PlaceShip(5, 1);
-    });
 
-    $("#ButtonCreateBoardMed").click(function () {
+		$("div").remove(".start");
+		CreateBoard(5);
+		PlaceShip(5, 1);
+	});
+
+	$("#ButtonCreateBoardMed").click(function () {
 		var name = $("#Name").val();
 		$("div").remove(".start");
 
-		
-        CreateBoard(10);
-        PlaceShip(10, 2)
 
-    });
+		CreateBoard(10);
+		PlaceShip(10, 2)
 
-    $("#ButtonCreateBoardLarge").click(function () {
+	});
+
+	$("#ButtonCreateBoardLarge").click(function () {
 		var name = $("#Name").val();
 		$("div").remove(".start");
 
+
+		CreateBoard(15);
+		PlaceShip(15, 5);
+	});
+
+	function CreateBoard(size) {
+		var row = "<div class='divTableRow'>";
+		var divEnd = "</div>";
+
+		for (var i = 0; i < size; i++) {
+			$("#divTable").append(row);
+			for (var j = 0; j < size; j++) {
+				var cellID = "cellx" + i +"y"+ j;
+				var col = "<div class='divTableCell' id='" + cellID + "'>Cell: x" + i + "y"+j + "</div>";
+				$("#divTable").append(col);
+			}
+			$("#divTable").append(divEnd);
+		}
+	}
+
+	function PlaceShip(size, shipQuantity) {
+		var shipArray = [shipQuantity];
+		var shipPlace;
+		for (var i = 0; i < shipQuantity; i++) {
+			var x = 0 + Math.floor(Math.random() * size);
+			var y = 0 + Math.floor(Math.random() * size);
+			shipPlace = "cellx" + x +"y"+ y;
+			var doppleGanger = false;
+			for (var j = 0; j < shipArray.length; j++) {
+
+				if (shipPlace == shipArray[j]) {
+					doppleGanger = true;
+				}
+			}
+			if (doppleGanger == true) {
+				i--;
+			}
+			else
+				shipArray[i] = shipPlace;
+		}
 		
-        CreateBoard(15);
-        PlaceShip(15, 5);
-    });
-
-    function CreateBoard(size) {
-        var row = "<div class='divTableRow'>";
-        var divEnd = "</div>";
-
-        for (var i = 0; i < size; i++) {
-            $("#divTable").append(row);
-            for (var j = 0; j < size; j++) {
-                var cellID = "cell" + i + j;
-                var col = "<div class='divTableCell' id='" + cellID + "'>Cell: " + i + j + "</div>";
-                $("#divTable").append(col);
-            }
-            $("#divTable").append(divEnd);
-        }
-    }
-
-    function PlaceShip(size, shipQuantity)
-    {
-        var shipArray = [x,x,x,x,x];
-        var shipPlace;
-        for (var i = 0; i < shipQuantity; i++) {
-            var x = 0 + Math.floor(Math.random() * size);
-            var y = 0 + Math.floor(Math.random() * size);
-            shipPlace = "cell" + x + y;
-            var doppleGanger = false;
-            for (var j = 0; j < shipArray.length; j++) {
-
-                if (shipPlace == shipArray[j]) {
-                    doppleGanger = true;
-                }
-            }
-            if (doppleGanger == true) {
-                i--;
-            }
-            else
-                shipArray[i] = shipPlace;
-        }
-        //alert(shipPlace);
-    }
-
-    $("#cell00").click(function () {
-        var cell00 = $("#cell00").text();
-        alert(cell00);
-    });
+	}
+		
 });
 
 
