63fd44f66ab58b416a401cfee12cc6293c5df1c7
Mattias Lij <mattias.lij@hotmail.com>
2017-10-06 10:55:44 +0200

    COMMIY ML

--- Fantastic4/JavaScript.js
+++ Fantastic4/JavaScript.js
@@ -9,7 +9,35 @@ $(document).ready(function () {
 
     $("#ButtonCreateBoardSmall").click(function () {
 
-        $("#divTable").append("<div>Test</div>");
+        var smallSize = 5;
+
+        var row = "<div class='divTableRow'>";
+        var col = "<div class='divTableCell' id='cell00'>&nbsp;</div>"
+        var divEnd = "</div>";
+
+        for (var i = 0; i < smallSize; i++) {
+
+        }
+
+        $("#divTable").append(row + col + col + col + col + col + divEnd + divEnd + divEnd + divEnd + divEnd + divEnd
+            + row + col + col + col + col + col + divEnd + divEnd + divEnd + divEnd + divEnd + divEnd + 
+            row + col + col + col + col + col + divEnd + divEnd + divEnd + divEnd + divEnd + divEnd + 
+            row + col + col + col + col + col + divEnd + divEnd + divEnd + divEnd + divEnd + divEnd + 
+            row + col + col + col + col + col + divEnd + divEnd + divEnd + divEnd + divEnd + divEnd
+        );
+
+
+        //$("#divTable").append("<div class='divTableCell'>Test</div><div class='divTableCell'>Test</div>");
+
+        //<div class="divTableRow">
+        //    <div class='divTableCell' id="cell00">&nbsp;</div>
+        //    <div class="divTableCell" id="cell01">&nbsp;</div>
+        //</div>
+        //    <div class="divTableRow">
+        //        <div class="divTableCell" id="cell10">&nbsp;</div>
+        //        <div class="divTableCell" id="cell11">&nbsp;</div>
+        //    </div>
+        
 
     });
 
