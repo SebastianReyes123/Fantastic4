82f728fcc95b7e674c027ac1439a7df8cc5dc8cd
Mattias Lij <mattias.lij@hotmail.com>
2017-10-06 16:03:53 +0200

    Mattias Final Edit

--- Fantastic4/JavaScript.js
+++ Fantastic4/JavaScript.js
@@ -1,74 +1,50 @@
-﻿$(document).ready(function () {
+﻿var shipArray;
+var counterShots = 0;
+var counterHit = 0;
 
-	$("#ButtonCreateBoardSmall").click(function () {
-		var name = $("#Name").val();
+$(document).ready(function () {
+    $("#endGameDIV").hide();
+    CreateBoard(5);
+    PlaceShip(5, 2);
 
-		$("div").remove(".start");
-		CreateBoard(5);
-		PlaceShip(5, 1);
-	});
+    //Test för att se om knappar funkar
+    //$("#cellx0y0").click(function () {
+    //    alert("Hej");
+    //});
 
-	$("#ButtonCreateBoardMed").click(function () {
-		var name = $("#Name").val();
-		$("div").remove(".start");
+    $("#ButtonCreateBoardSmall").click(function () {
+        var name = $("#Name").val();
+        $("div").remove(".start");
+
+        CreateBoard(5);
+        PlaceShip(5, 1);
+    });
+
+    $("#ButtonCreateBoardMed").click(function () {
+        var name = $("#Name").val();
+        $("div").remove(".start");
+
+        CreateBoard(10);
+        PlaceShip(10, 2)
+    });
+
+    $("#ButtonCreateBoardLarge").click(function () {
+        var name = $("#Name").val();
+        $("div").remove(".start");
 
 
-		CreateBoard(10);
-		PlaceShip(10, 2)
+        CreateBoard(15);
+        PlaceShip(15, 5);
+    });
 
-	});
+    function CreateBoard(size) {
+        var row = "<div class='divTableRow'>";
+        var divEnd = "</div>";
 
-	$("#ButtonCreateBoardLarge").click(function () {
-		var name = $("#Name").val();
-		$("div").remove(".start");
-
-
-		CreateBoard(15);
-		PlaceShip(15, 5);
-	});
-
-	function CreateBoard(size) {
-		var row = "<div class='divTableRow'>";
-		var divEnd = "</div>";
-
-		for (var i = 0; i < size; i++) {
-			$("#divTable").append(row);
-			for (var j = 0; j < size; j++) {
-				var cellID = "cellx" + i +"y"+ j;
-				var col = "<div class='divTableCell' id='" + cellID + "'>Cell: x" + i + "y"+j + "</div>";
-				$("#divTable").append(col);
-			}
-			$("#divTable").append(divEnd);
-		}
-	}
-
-	function PlaceShip(size, shipQuantity) {
-		var shipArray = [shipQuantity];
-		var shipPlace;
-		for (var i = 0; i < shipQuantity; i++) {
-			var x = 0 + Math.floor(Math.random() * size);
-			var y = 0 + Math.floor(Math.random() * size);
-			shipPlace = "cellx" + x +"y"+ y;
-			var doppleGanger = false;
-			for (var j = 0; j < shipArray.length; j++) {
-
-				if (shipPlace == shipArray[j]) {
-					doppleGanger = true;
-				}
-			}
-			if (doppleGanger == true) {
-				i--;
-			}
-			else
-				shipArray[i] = shipPlace;
-		}
-		
-	}
-		
         for (var i = 0; i < size; i++) {
             $("#divTableID").append(row);
             for (var j = 0; j < size; j++) {
-                var cellID = "cell" + i + j;
+                var cellID = "cellx" + i + "y" + j;
                 var col = "<div class='divTableCell' id='" + cellID + "'></div>";
                 $("#divTableID").append(col);
             }
@@ -76,26 +52,58 @@
         }
     }
 
-    function checkIfHit(shotX, shotY)
-    {
+    function PlaceShip(size, shipQuantity) {
+        shipArray = [shipQuantity];
+        var shipPlace;
+        for (var i = 0; i < shipQuantity; i++) {
+            var x = 0 + Math.floor(Math.random() * size);
+            var y = 0 + Math.floor(Math.random() * size);
+            shipPlace = "cellx" + x + "y" + y;
+            var doppleGanger = false;
+            for (var j = 0; j < shipArray.length; j++) {
+
+                if (shipPlace == shipArray[j]) {
+                    doppleGanger = true;
+                }
+            }
+            if (doppleGanger == true) {
+                i--;
+            }
+            else
+                shipArray[i] = shipPlace;
+        }
+
+    }
+
+    function checkIfHit(cellID) {
         var nrShips = shipArray.length;
+        counterShots++;
 
         for (var i = 0; i < nrShips; i++) {
-            var shipY = shipArray[i][5];
-            var shipX = shipArray[i][7];
+            if (shipArray[i] == cellID) {
+                alert("Träff!");
+                $("#" + cellID).css("background-color", "blue");
+                counterHit++;
+                if (counterHit == nrShips)
+                {
+                    alert("GRATTIS! Du klarade spelet på " + counterShots + " försök");
+                    $("#playGameDIV").hide();
+                    $("#endGameDIV").fadeIn();
+                }
+            }
         }
-    };
+    }
 
     $("#divTableID .divTableCell").click(function () {
         $(this).css("background-color", "red");
 
         var cellID = $(this).attr('id');
-        var cellx = cellID[5]
-        var celly = cellID[4]
+        var cellx = cellID[7];
+        var celly = cellID[5];
 
         //alert(celly + cellx);
 
-        checkIfHit(cellx, celly);
+        checkIfHit(cellID);
     });
 
 });
